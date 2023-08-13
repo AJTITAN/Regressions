ceed4665bcaa74e93661951088ac769cca1fc7b7
Ashish Joshi <97600247+AJTITAN@users.noreply.github.com>
2023-08-13 16:28:09 +0000

    done

--- Linear_Regression_speed.py
+++ Linear_Regression_speed.py
@@ -1,13 +1,8 @@
-
-import sys
-import matplotlib
-matplotlib.use('Agg')
-
 import matplotlib.pyplot as plt
 from scipy import stats
 
-x = [89,43,36,36,95,10,66,34,38,20,26,29,48,64,6,5,36,66,72,40]
-y = [21,46,3,35,67,95,53,72,58,10,26,34,90,33,38,20,56,2,47,15]
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
 slope, intercept, r, p, std_err = stats.linregress(x, y)
 
@@ -20,6 +15,41 @@ plt.scatter(x, y)
 plt.plot(x, mymodel)
 plt.show()
 
-#Two  lines to make our compiler able to draw:
-plt.savefig(sys.stdout.buffer)
-sys.stdout.flush()
+
+
+
+'''
+mport matplotlib.pyplot as plt
+from scipy import stats
+
+Create the arrays that represent the values of the x and y axis:
+
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+Execute a method that returns some important key values of Linear Regression:
+
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+
+Create a function that uses the slope and intercept values to return a new value. This new value represents where on the y-axis the corresponding x value will be placed:
+
+def myfunc(x):
+  return slope * x + intercept
+
+Run each value of the x array through the function. This will result in a new array with new values for the y-axis:
+
+mymodel = list(map(myfunc, x))
+
+Draw the original scatter plot:
+
+plt.scatter(x, y)
+
+Draw the line of linear regression:
+
+plt.plot(x, mymodel)
+
+Display the diagram:
+
+plt.show()
+
+'''
